2aee9167228d1fc722625cf0af95ab8a69b475bc
MOURAD <MOURAD@TabbiNesrine>
2023-11-08 15:27:26 +0100

    new msg

--- EXO1/src/exo1/HelloWorld.java
+++ EXO1/src/exo1/HelloWorld.java
@@ -3,5 +3,6 @@ package exo1;
 public class HelloWorld {
 	 public static void main(String[] args) {
 	 System.out.println("hello world!");
+	 System.out.println("Je suis une version modifiée du code de l'exo 1");
 	 }
 }
